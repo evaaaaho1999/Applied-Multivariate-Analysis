bcfc2ea4db3a6f52e1dfa3e4c6bb452f5203a7e4
I Hua Ho <89164829+evaaaaho1999@users.noreply.github.com>
2021-09-23 23:22:55 -0500

    Delete hw1.R

--- hw1.R
+++
@@ -1,9 +0,0 @@
-install.packages("openxlsx")
-library("openxlsx")
-p2<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=2)
-p3<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=3)
-p4<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=4)
-p5<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=5)
-p6<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=6)
-p7<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=7)
-p8<- read.xlsx(xlsxFile="C:/Users/eva/Downloads/collegescorecarddatadictionary_01192021.xlsx", sheet=8)p6
